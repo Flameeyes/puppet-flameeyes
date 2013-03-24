ec452e98865a04e8d816c83d47b6762c13a0e2af
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2013-03-24 10:56:10 -0700

    Add my default tmux config as well.

---
+++ manifests/tmux.pp
@@ -0,0 +1,20 @@
+class tmux {
+  case $::osfamily {
+    default: {
+      case $::operatingsystem {
+        "Gentoo": {
+	  $package = "app-misc/tmux"
+        }
+      }
+    }
+  }
+
+  package { $package:
+    ensure => installed
+  }
+
+  file { '/etc/tmux.conf':
+    ensure => file,
+    source => "puppet:///modules/flameeyes/tmux.conf",
+  }
+}
