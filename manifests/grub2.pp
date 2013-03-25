1e16bb5caafad9860a60b0689b2567a704f85e62
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2013-03-25 04:26:16 -0700

    Use portage::package.

--- manifests/grub2.pp
+++ manifests/grub2.pp
@@ -1,9 +1,11 @@
 class flameeyes::grub2 {
   if $::operatingsystem == "Gentoo" {
-    package_keywords { 'sys-boot/grub': version => "=2*", keywords => "**", }
-    package_unmask { 'sys-boot/grub': version => "=2*", }
-
-    package { 'sys-boot/grub': version => "=2*", ensure => installed, }
+    portage::package { 'sys-boot/grub':
+      ensure => "=2*",
+      keywords => "**",
+      keywords_version => "=2*",
+      unmask => "=2*"
+    }
 
     file { '/etc/kernel/postinst.d/99grub2':
       ensure => file,
