0717c20bff53067c3e052be2400752a4128747eb
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2013-03-25 06:01:12 -0700

    Avoid using the 2* version spec for grub.

--- manifests/grub2.pp
+++ manifests/grub2.pp
@@ -1,7 +1,7 @@
 class flameeyes::grub2 {
   if $::operatingsystem == "Gentoo" {
     portage::package { 'sys-boot/grub':
-      ensure => "2*",
+      ensure => installed,
       keywords => "**",
       keywords_version => "=2*",
       unmask => "=2*"
